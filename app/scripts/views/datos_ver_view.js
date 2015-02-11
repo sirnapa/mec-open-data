44c620cc562b2e8af08a02e2d24ccb9993e4cec6
Nelson Páez <nelpa90@gmail.com>
2015-02-11 10:42:22 -0300

    Filtros para tabla y sticky table. Corrección del label en Ver columnas. Estilo para menú activo.

--- app/scripts/views/datos_ver_view.js
+++ app/scripts/views/datos_ver_view.js
@@ -8,6 +8,30 @@ Mec.DatosVerView = Ember.View.extend({
         $(botones_derecha[1]).html('Ver columnas');
         $('.btn-toolbar .focus-btn-group button').html('<span class="glyphicon glyphicon-screenshot"></span> Resaltar');
 
+
+        var tabla_original = tabla.find('div').next('table');
+        var filtros_clonados = tabla.find('table').first().find('thead tr th');
+        var titulos = [];
+
+        tabla_original.find('thead tr th').each(function(t,th){
+            titulos.push($(th).find('span[data-titulo=true]').html());
+
+            // De original a clonado
+            $(th).find('input,select').keyup(function(){
+                $(filtros_clonados[t]).find('input,select').val($(this).val());
+            });
+
+            // De clonado a original
+            $(filtros_clonados[t]).find('input,select').keyup(function(){
+                $(th).find('input,select').val($(this).val());
+            });
+
+        });
+
+        $(botones_derecha[1]).next().find('li').each(function(l,li){
+            $(li).find('label').html(titulos[l]);
+        });
+
         $('.orden').click(function(){
 
         	// El comportamiento del botón es:
