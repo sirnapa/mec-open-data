f2304a45c98c605fdf401348bdee5bdf9ebe5b9c
Nelson Páez <nelpa90@gmail.com>
2015-02-12 11:11:28 -0300

    URI de nómina

---
+++ app/scripts/views/uri_nomina_view.js
@@ -0,0 +1,17 @@
+Mec.UriNominaView = Ember.View.extend({
+    didInsertElement : function(){
+        this._super();
+
+        window.redimensionar = function(){
+
+          $('.listado li').each(function(f,fila){
+            $(fila).css('height','auto');
+            $(fila).css('height',$(fila).height());
+          });
+
+        };
+
+        $('body').attr('onresize','redimensionar();');
+        window.redimensionar();
+	}
+});
